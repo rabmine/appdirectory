741eb977cecdee4c845d9acb5d88612b0d80a5e6
Facundo Olano <facundo.olano@gmail.com>
2012-02-28 17:13:39 -0300

    category highlight

--- project/app/templatetags/tags.py
+++ project/app/templatetags/tags.py
@@ -4,7 +4,7 @@ from app.models import CATEGORIES, Application
 register = template.Library()
 
 @register.inclusion_tag('site/sidebar.html')
-def sidebar():
+def sidebar(selected=None):
     
     app_count = Application.objects.count()
     iphone_count = Application.objects.apps_by_device('iphone').count()
@@ -19,4 +19,5 @@ def sidebar():
     return {'categories' : categories,
             'app_count' : app_count,
             'iphone_count' : iphone_count,
-            'ipad_count' : ipad_count}
+            'ipad_count' : ipad_count,
+            'selected' : selected}
--- project/app/views.py
+++ project/app/views.py
@@ -2,16 +2,9 @@ from django.views.generic.list import ListView
 from django.conf import settings
 from django.db.models import Q
 from django.shortcuts import render_to_response, get_object_or_404
-from django.views.generic import TemplateView
 
 from models import Application
 
-class HomeView(TemplateView):
-    template_name = "index.html"
-
-class AppDetailView(TemplateView):
-    template_name = "app_detail.html"
-    
 class BaseAppListView(ListView):
     """ Base view for application lists on the main page. """
     
@@ -23,7 +16,7 @@ class BaseAppListView(ListView):
 class DeviceAppListView(BaseAppListView):
     def get_context_data(self, **kwargs):
         
-        context =  ListView.get_context_data(self, **kwargs)
+        context =  super(DeviceAppListView, self).get_context_data(**kwargs)
         context['device'] = self.kwargs['device']
         return context
     
@@ -42,6 +35,12 @@ class SearchAppListView(BaseAppListView):
 
 class CategoryAppListView(BaseAppListView):
     
+    def get_context_data(self, **kwargs):
+        
+        context =  super(CategoryAppListView, self).get_context_data(**kwargs)
+        context['selected_category'] = self.kwargs['category']
+        return context
+    
     def get_queryset(self):
         category = self.kwargs['category']
         return Application.objects.apps_by_category(category)
@@ -52,7 +51,8 @@ class ArtistAppListView(BaseAppListView):
         artist_name = self.kwargs['artist_name']
         return Application.objects.filter(artist_name=artist_name)
 
+#FIXME make class based
 def detail(request, id):
     application = get_object_or_404(Application, application_id=id)
-    view_data = dict(app=application)
+    view_data = dict(app=application, selected_category=application.get_category())
     return render_to_response("app_detail.html", view_data)
