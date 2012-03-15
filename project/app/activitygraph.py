f9828360fdd92c8b3b8889c16c7ca75baa22c35c
Facundo Olano <facundo.olano@gmail.com>
2012-03-15 16:39:51 -0300

    activity changes

--- project/app/activitygraph.py
+++ project/app/activitygraph.py
@@ -6,39 +6,26 @@ from django.http import HttpResponse
 import json
 from app.models import Application
 from app.constants import USA_STOREFRONT
+import time
 
 class ActivityGraphView(TemplateView):
     """ Base class for activity graph views. """
     
-    def get_yaxis(self):
-        return ""
+    def now_timestamp(self):
+        return time.time() * 1000
     
-    def patch_max(self, context):
-        """ 
-        Checks if all y points are equal, and adds a max flag to avoid jqplot
-        drawing a constant line at the top of the graph.
-        """
-        
-        if context['data']:
-            value = context['data'][0][1]
-            constant = True
-            
-            for _, v in context['data']:
-                if v != value:
-                    constant = False
-                    break
-        
-            if constant:
-                context.update({'max' : value * 2 })
+    def get_yaxis(self):
+        return {}
     
     def get_context_data(self, **kwargs):
         app_id=int(self.kwargs['app_id'])
         app = Application.objects.get(application_id=app_id)
         
-        context = {'data' : self.get_data_points(app),
-                   'yaxis_format' : self.get_yaxis_format()}
+        data = self.get_data_points(app)
         
-        self.patch_max(context)
+        context = {'data' : data,
+                   'title' : self.get_title(),
+                   'yaxis' : self.get_yaxis(data)}
         
         return context
     
@@ -49,6 +36,9 @@ class ActivityGraphView(TemplateView):
 
 class PriceGraphView(ActivityGraphView):
     
+    def get_title(self):
+        return "App price"
+    
     def get_data_points(self, app):
         values = app.applicationhistory_set.values_list('export_date', 'retail_price')
         
@@ -57,17 +47,54 @@ class PriceGraphView(ActivityGraphView):
         
         price = app.price(USA_STOREFRONT)
         if price:
-            points.append([app.export_date, float(str(price)) if price else 0])
+            points.append([self.now_timestamp(), float(str(price)) if price else 0])
         
         return points
     
-    def get_yaxis_format(self):
-        return "$%.2f"
+    def patch_max(self, data):
+        """ 
+        Checks if all y points are equal, and adds a max flag to avoid jqplot
+        drawing a constant line at the top of the graph.
+        """
+        
+        if data:
+            value = data[0][1]
+            for _, v in data:
+                if v != value:
+                    return False
+        
+            return True
+    
+    
+    def get_yaxis(self, data):
+        
+        yaxis = {'tickOptions': {
+                                'formatString' : "$%.2f",
+                                }
+                }
+        
+        if self.patch_max(data):
+            yaxis['max'] = data[0][1] * 2
+            yaxis['min'] = 0
+        
+        return yaxis
 
 class VersionGraphView(ActivityGraphView):
-    pass
+    
+    def get_title(self):
+        return "Version"
+    
 
 class Top250GraphView(ActivityGraphView):
+    
+    def get_title(self):
+        return "Top 250"
+    
+    def get_yaxis(self, data):
+        return {'max' : 1, 'min' : 250,
+                'tickOptions': {
+                                'formatString' : "#%d",
+                                }}
     
     def get_data_points(self, app):
         values = app.applicationhistory_set.values_list('export_date', 'application_rank')
@@ -75,6 +102,6 @@ class Top250GraphView(ActivityGraphView):
         
         current_rank = app.rank()
         if current_rank and current_rank <= 250:
-            points.append([app.export_date, current_rank])
+            points.append([self.now_timestamp(), current_rank])
         
         return points
